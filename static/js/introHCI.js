ffb1517ed438cba3036300a99ea9039962d837da
Chonlada Woointranon <cwoointr@gmail.com>
2016-01-21 11:42:17 -0800

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -9,10 +9,44 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+		$("#testjs").click(function(e) {
+			$('.jumbotron h1').text("Javascript is connected");
+			$("#testjs").text("HI HI! Please wait...");
+			$(".jumbotron p").toggleClass("active");
+		});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("#submitBtn").click(updateProject);
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+  // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the leement that fired the event
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+		var containingProject = $(this).closest(".project");
+		var description = $(containingProject).find(".project-description");
+		  if (description.length == 0) {
+		  	$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+		  } else {
+		    description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+		  }
+}
+
+function updateProject(e) {
+	 //get project title to change
+   var projectID = $('#project').val();
+	 //change the width value to certain val
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+
+	 //change the text to whatever text
+   var newText=$('#description').val();
+   $(projectID+" .project-description").text(newText);
 }
